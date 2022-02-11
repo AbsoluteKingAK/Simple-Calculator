2089265738abf31c0f92f10022c51f76680e26eb
AbsoluteKingAK <99478612+AbsoluteKingAK@users.noreply.github.com>
2022-02-11 16:44:13 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+print("Simple Calculator")
+y = 10
+z = 5
+add = y+z
+sub = y-z
+mul = y*z
+quo = y/z
+rem = y%z
+print("The sum of",y,"and",z,"is",add)
+print("The diffrence of",y,"and",z,"is",sub)
+print("The product of",y,"and",z,"is",mul)
+print("The quotient of",y,"and",z,"is",quo)
+print("The reminder of",y,"and",z,"is",rem)
